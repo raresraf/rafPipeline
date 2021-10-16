a283b1a67926cf9f2b44cdf1bfb89121594c82df
raresraf <raresfolea97@gmail.com>
2021-10-16 14:45:40 +0300

    all feature

--- run_pipeline/run_on_demand.py
+++ run_pipeline/run_on_demand.py
@@ -7,7 +7,7 @@ TIMEOUT_RUN_ON_DEMAND = 10 * 60
 
 
 def run_pipeline(atomic, solution_path, problemID):
-    print(f"{problemID}: Running pipeline for {solution_path}")
+    print(f"{problemID}: Running pipeline for {solution_path}. Atomic: {atomic}")
     for root, dirs, files in os.walk(f"../../TheInputsCodeforces/{problemID}"):
         for name in files:
             if '.DAT' not in name:
@@ -21,14 +21,15 @@ def run_pipeline(atomic, solution_path, problemID):
             try:
                 subprocess.call([f"./atomics/run_atomic_{atomic}.sh", solution_path, input_path, output_path],
                                 shell=False, timeout=TIMEOUT_RUN_ON_DEMAND)
-            except Exception:
-                pass
+            except Exception as e:
+                print(f"Exception for {output_path}: {e}")
 
 
 if __name__ == "__main__":
     print("===== Run on-demand pipeline script =====")
     if len(sys.argv) != 3:
         print("Usage: python3 run_on_demand <problemID> <atomic_name>")
+        print("Usage: <atomic_name> = 'all' to run all")
         sys.exit(1)
 
     problemID = sys.argv[1]
@@ -40,5 +41,10 @@ if __name__ == "__main__":
             sys.exit(1)
 
         for solution_path in metadata.get(problemID, None):
-            # remove .CPP
-            run_pipeline(atomic, solution_path[:-4], problemID)
+            if "all" == atomic.lower():
+                # remove .CPP
+                run_pipeline("perf", solution_path[:-4], problemID)
+                run_pipeline("time", solution_path[:-4], problemID)
+            else:
+                # remove .CPP
+                run_pipeline(atomic, solution_path[:-4], problemID)
