1943b121fa9d6505de4f786ec175b64e4a8f1a8f
raresraf <raresfolea97@gmail.com>
2021-10-22 21:19:28 +0300

    driver -> demo rebrand

--- rComplexity/demo.py
+++ rComplexity/demo.py
@@ -5,11 +5,9 @@ import numpy as np
 from rComplexity.features.get_best_feature import get_best_feature
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
-        print("Usage: python3 driver.py <path_to_metrics> ")
+    if len(sys.argv) != 1:
+        print("Usage: python3 demo.py")
         sys.exit(-1)
-
-    path = sys.argv[1]
 
     X = np.array([1, 2, 3, 4])
     y = np.array([0, 1.2, 4.3, 9.6])
